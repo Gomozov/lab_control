6566c3f140e3eb751d0bc9511792fe77bdeca2a5
Gomozov <Valeriy.gomozov@gmail.com>
2012-08-21 13:50:31 +0400

    initial point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabControl::Application.config.secret_token = '38bd0c93d03661a489b11208f2418039a894dcc16363306fb2dce17872588605ef777d85bbffced0489c4390c9c7f6176ad25bb411a3d0bc2a67acd2aebf977d'
